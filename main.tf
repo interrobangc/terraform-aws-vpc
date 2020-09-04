8f91535c606eb77a0beb6be6face6043e208305e
Robert Bo Davis <bo@interrobang.consulting>
2020-09-04 13:06:26 -0400

    fixes for .12

--- main.tf
+++ main.tf
@@ -10,17 +10,17 @@ module "vpc" {
   name = "${var.env}-vpc"
   cidr = var.cidr
 
-  azs                     = [var.azs]
-  private_subnets         = [var.private_subnets]
-  public_subnets          = [var.public_subnets]
+  azs                     = var.azs
+  private_subnets         = var.private_subnets
+  public_subnets          = var.public_subnets
   map_public_ip_on_launch = var.map_public_ip_on_launch
 
   create_database_subnet_group = var.create_database_subnet_group
-  database_subnets             = [var.database_subnets]
+  database_subnets             = var.database_subnets
 
-  redshift_subnets    = [var.redshift_subnets]
-  elasticache_subnets = [var.elasticache_subnets]
-  intra_subnets       = [var.intra_subnets]
+  redshift_subnets    = var.redshift_subnets
+  elasticache_subnets = var.elasticache_subnets
+  intra_subnets       = var.intra_subnets
 
   enable_nat_gateway = var.enable_nat_gateway
   single_nat_gateway = var.single_nat_gateway
