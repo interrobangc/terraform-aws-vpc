b4d9a13740afc7d8c1ede8a9bfa6614ca98c59f3
Robert Bo Davis <bo@interrobang.consulting>
2020-09-08 09:05:55 -0400

    tf 13 updates

--- output.tf
+++ output.tf
@@ -1,16 +1,16 @@
 output "public_subnets" {
   description = "List of IDs of public subnets"
-  value       = [module.vpc.public_subnets]
+  value       = module.vpc.public_subnets
 }
 
 output "private_subnets" {
   description = "List of IDs of private subnets"
-  value       = [module.vpc.private_subnets]
+  value       = module.vpc.private_subnets
 }
 
 output "database_subnets" {
   description = "List of IDs of database subnets"
-  value       = [module.vpc.database_subnets]
+  value       = module.vpc.database_subnets
 }
 
 output "database_subnet_group" {
@@ -20,7 +20,7 @@ output "database_subnet_group" {
 
 output "elasticache_subnets" {
   description = "List of IDs of elasticache subnets"
-  value       = [module.vpc.elasticache_subnets]
+  value       = module.vpc.elasticache_subnets
 }
 
 output "elasticache_subnet_group" {
@@ -35,12 +35,12 @@ output "elasticache_subnet_group_name" {
 
 output "intra_subnets" {
   description = "List of IDs of intra subnets"
-  value       = [module.vpc.intra_subnets]
+  value       = module.vpc.intra_subnets
 }
 
 output "redshift_subnets" {
   description = "List of IDs of redshift subnets"
-  value       = [module.vpc.redshift_subnets]
+  value       = module.vpc.redshift_subnets
 }
 
 output "id" {
@@ -50,20 +50,20 @@ output "id" {
 
 output "public_route_table_ids" {
   description = "List of IDs of public route tables"
-  value       = [module.vpc.public_route_table_ids]
+  value       = module.vpc.public_route_table_ids
 }
 
 output "private_route_table_ids" {
   description = "List of IDs of private route tables"
-  value       = [module.vpc.private_route_table_ids]
+  value       = module.vpc.private_route_table_ids
 }
 
 output "vpc_main_route_table_id" {
   description = "The ID of the main route table associated with this VPC"
-  value       = [module.vpc.vpc_main_route_table_id]
+  value       = module.vpc.vpc_main_route_table_id
 }
 
 output "natgw_ids" {
   description = "List of NAT Gateway IDs"
-  value       = [module.vpc.natgw_ids]
+  value       = module.vpc.natgw_ids
 }
